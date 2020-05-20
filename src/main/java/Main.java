2fd52eb6314941c4092f21545c503696d4d83ff4
Jacob Bosco <jacob.bosco3075@gmail.com>
2020-05-20 21:50:38 +0530

    Created simple mock tests in Main.java file and updated Mall class respectively. Updating pom file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,35 @@
+import shops.ClothsShop;
+import shops.Shop;
+import floor.Floor;
+import floor.NormalFloor;
+
+import java.util.List;
+
 public class Main {
 	public static void main(String[] args) {
+		List<Shop> shops1 = List.of(
+				new ClothsShop("Shop1", 100, 150, 50),
+				new ClothsShop("Shop2", 120, 170, 40)
+		);
+		List<Shop> shops2 = List.of(
+				new ClothsShop("Shop3", 100, 150, 20),
+				new ClothsShop("Shop4", 120, 170, 50)
+		);
+
+		Floor floor1 = new NormalFloor(shops1);
+		Floor floor2 = new NormalFloor(shops2);
+
+		List<Floor> floors = List.of(floor1, floor2);
+
+		Mall mall = new Mall(floors, 1000);
+
+		System.out.println("mall.getTotalRevenue() = " + mall.getTotalRevenue());
+		mall
+				.getFloors()
+				.stream()
+				.map((Floor::getFreeSpace))
+				.filter(floorSpace -> floorSpace < 100)
+				.forEach(System.out::println);
+		System.out.println("mall = " + mall);
 	}
 }
--- src/main/java/Mall.java
+++ src/main/java/Mall.java
@@ -1,16 +1,17 @@
+import floor.Floor;
+
 import java.util.List;
 
 public class Mall {
 	private List<Floor> floors;
+	private int revenue;
+	private final int operationCost;
 
-	public Mall() {
 
-	}
-
-	public Mall(List<Floor> floors) {
+	public Mall(List<Floor> floors, int operationCost) {
 		this.floors = floors;
+		this.operationCost = operationCost;
 	}
-
 
 	public Mall addFloor(Floor newFloor) {
 		this.floors.add(newFloor);
@@ -24,5 +25,28 @@ public class Mall {
 	public Mall setFloors(List<Floor> floors) {
 		this.floors = floors;
 		return this;
+	}
+
+	public int getTotalRevenue() {
+		if (floors == null) return -1;
+		return floors.stream()
+				.map(Floor::getRevenue)
+				.mapToInt(floorRevenue -> floorRevenue)
+				.sum();
+	}
+
+	@Override
+	public String toString() {
+		return "Mall{" +
+				"floors=" + floors +
+				'}';
+	}
+
+	public int getRevenue() {
+		return revenue;
+	}
+
+	public int getOperationCost() {
+		return operationCost;
 	}
 }
