35cb7d5cde2586ac2597be419b6b4660d22a4ebc
Jacob Bosco <jacob.bosco3075@gmail.com>
2020-05-20 21:44:45 +0530

    Converted Floor class to an interface and created concrete implementation with NormalFloor class

---
+++ src/main/java/floor/Floor.java
@@ -0,0 +1,28 @@
+package floor;
+
+import Shops.Shop;
+import utils.FloorsStream;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public interface Floor {
+
+	List<Shop> getShops();
+
+	NormalFloor addShop(Shop shop);
+
+	int getFreeSpace();
+
+	int getTotalSpace();
+
+	int getRevenue();
+
+	static FloorsStream stream(List<Floor> floors) {
+		return new FloorsStream(floors.stream());
+	}
+
+	default FloorsStream with(Floor floor) {
+		return new FloorsStream(Stream.of(this, floor));
+	}
+}
---
+++ src/main/java/floor/NormalFloor.java
@@ -0,0 +1,64 @@
+package floor;
+
+import Shops.Shop;
+
+import java.util.List;
+
+public class NormalFloor implements Floor{
+	private final int totalSpace = 100;
+	private List<Shop> shops;
+	private int revenue;
+
+	public NormalFloor() {
+
+	}
+
+	public NormalFloor(List<Shop> shops) {
+		this.shops = shops;
+	}
+
+	@Override
+	public List<Shop> getShops() {
+		return shops;
+	}
+
+
+	@Override
+	public NormalFloor addShop(Shop shop) {
+		this.shops.add(shop);
+		return this;
+	}
+
+	@Override
+	public int getFreeSpace() {
+		int usedSpace = shops
+				.stream()
+				.map(Shop::getShopFloorArea)
+				.mapToInt(floorArea -> floorArea)
+				.sum();
+		return totalSpace - usedSpace;
+	}
+
+	@Override
+	public int getTotalSpace() {
+		return totalSpace;
+	}
+
+	@Override
+	public int getRevenue() {
+		return shops
+				.stream()
+				.map(Shop::getRevenue)
+				.mapToInt(shopRevenue -> shopRevenue)
+				.sum();
+	}
+
+	@Override
+	public String toString() {
+		return "\n\tNormalFloor{" +
+				"totalSpace=" + totalSpace +
+				", revenue=" + revenue +
+				", shops=" + shops +
+				'}';
+	}
+}
